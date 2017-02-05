121355f2a11af2f3b7cf5983a792733c1327f6d1
Alberto Madrazo <betomadrazo@gmail.com>
2017-02-04 19:45:24 -0600

    Crea metodo buscarRepos para que la busqueda inicie o apretando el boton de buscar o el boton del teclado.

--- app/src/main/java/com/albertomadrazo/android/gitlook/activity/MainActivity.java
+++ app/src/main/java/com/albertomadrazo/android/gitlook/activity/MainActivity.java
@@ -3,10 +3,13 @@ package com.albertomadrazo.android.gitlook.activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.albertomadrazo.android.gitlook.R;
@@ -29,15 +32,16 @@ public class MainActivity extends AppCompatActivity {
         btn_searchLang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-
-                Bundle bundle = new Bundle();
-                bundle.putString("lenguaje", et_lenguaje.getText().toString());
-
-                RepositoriosListaFragment fragment = new RepositoriosListaFragment();
-                fragment.setArguments(bundle);
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLista, fragment).commit();
+                buscarRepos();
+            }
+        });
+        btn_searchLang.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if((keyEvent != null && keyEvent.getKeyCode() == keyEvent.KEYCODE_ENTER) || (i == EditorInfo.IME_ACTION_DONE)){
+                    buscarRepos();
+                }
+                return false;
             }
         });
 
@@ -45,5 +49,17 @@ public class MainActivity extends AppCompatActivity {
             RepositoriosListaFragment listaClientes = new RepositoriosListaFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.fragmentLista , listaClientes, LISTA_FRAGMENT_TAG).commit();
         }
+    }
+
+    private void buscarRepos(){
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("lenguaje", et_lenguaje.getText().toString());
+
+        RepositoriosListaFragment fragment = new RepositoriosListaFragment();
+        fragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLista, fragment).commit();
     }
 }
